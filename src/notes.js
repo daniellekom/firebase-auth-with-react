eb979adcf03e9b693933131239b64dd0edaf65cd
Danielle Kom <daniellekom1@gmail.com>
2022-02-16 10:33:39 -0500

    setup sign up page and routing

---
+++ src/notes.js
@@ -0,0 +1,16 @@
+//create a scenes folder w files inside
+//in index.js import {broswer router}
+//get route info for app.js from reactrouter.com
+//wrap <BrowserRouter> directions from reactrouter.com
+//create a scenes folder w login/sign/welcome and fill in
+//in app.js import login/signup/welcome && create routes
+//import use state in app.js to get user
+//inside welcome route do turnery for app.js
+
+//create a form in signup and login page (copy paste from 1 to the other)
+//create a label for signup.js
+//what do we want to do on react for form on signup? import usestate
+//create const for email and password and add onchange and value
+//onchange is what lets you plug in the "todd" in email 
+//create a handleFormsubmit w preventdefault so it wont reload in signup.js
+//add onsubmit={handleFormSubmit} on form element so form will work and not just reload
---
+++ src/scenes/Welcome.js
@@ -0,0 +1,15 @@
+
+export default function Welcome(){
+    return <h1>Welcome</h1>
+}
+
+
+//OLD WAY
+// import React from "react";
+// function Welcome(){
+// return (
+// <>
+// <h1>Welcome</h1>
+// </>
+// )}
+// export default Welcome;
